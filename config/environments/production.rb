d191d98397305ec8a0ed1331d34ea90a9dbec2f9
Chip Hayner <chiphayner@gmail.com>
2023-12-10 15:29:52 -0600

    updated permitted domains

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.hosts = [
     "*.herokuapp.com",
     "bucapandgown.com",
-    "belmont-cap-and-gown*.herokuapp.com",
+    "belmont-cap-and-gown-ed8fb27c409f.herokuapp.com",
     "infinite-meadow-09480.herokuapp.com",
     ENV["SERVER_HOST_NAME"],
     "belmontalum.chip.fyi"
