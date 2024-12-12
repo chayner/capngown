c1e35af7dbaa85aa139c8ac39b069864c61dcedb
Chip Hayner <chiphayner@gmail.com>
2024-12-12 16:43:15 -0600

    removed unneeded prod settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-  config.assets.js_compressor = :terser
-  config.assets.compile = false
-  config.assets.digest = true
-
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
